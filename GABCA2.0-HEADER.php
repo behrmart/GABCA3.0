881c1dbafebef4b0e347aefd512054f716e4f07f
behrmart <bfelipemm@gmail.com>
2026-05-18 17:14:04 -0600

    Changed Font to Google Quicksand

--- GABCA2.0-HEADER.php
+++ GABCA2.0-HEADER.php
@@ -27,7 +27,7 @@
 	<!-- Google Fonts Barlow -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet"> 
 
 
 
